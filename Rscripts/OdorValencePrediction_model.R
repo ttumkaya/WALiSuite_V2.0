8c4f2d654efdf6569fce6db58dd90fb4b29180d2
ttumkaya <tayfuntumkaya@gmail.com>
2020-08-21 11:13:36 +0800

    R script updated

--- Rscripts/OdorValencePrediction_model.R
+++ Rscripts/OdorValencePrediction_model.R
@@ -1,7 +1,7 @@
 library(caret)
-library(boot)
-library(MLmetrics)
-library(ROCR)
+#library(boot)
+#library(MLmetrics)
+#library(ROCR)
 
 df<-read.csv("/Users/Tayfun/Desktop/Codes/Odor_val_prediction_R/Transformed_mean_data_odor_prediction.csv")
 df_minimized<-df[,c(3, 6:28)]
@@ -78,24 +78,37 @@ plot(perf)
 getModelInfo(model)
 ## install.packages('LiblineaR', repo = "https://mac.R-project.org")
 
-
-### Playing around
-featurePlot(x=df_minimized[-1])
-
 ### Linear Regression Model
-training_data<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/Plots/LinearRegression/training_8LVs.csv")
+training_data_4LVs<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/PLSDA_components/PLSDA_4LVs.csv")
+training_data_6LVs<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/PLSDA_components/PLSDA_6LVs.csv")
+training_data_8LVs<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/Plots/LinearRegression/training_8LVs.csv")
+training_data_10LVs<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/PLSDA_components/PLSDA_10LVs.csv")
+training_data_12LVs<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/PLSDA_components/PLSDA_12LVs.csv")
+training_data_all<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/SSR_dataset.csv")
+
 target_data<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/Plots/LinearRegression/target.csv")
 weights<-read.csv("/Users/Tayfun/Desktop/Codes/OdorValPrediction/Plots/LinearRegression/weights.csv")
 names(weights)[1]<-'weights'
 
-regressionDF<-training_data
-regressionDF$valence<-target_data$Valence
-regressionDF$weight<-weights$weights
+regressionDF<-training_data_8LVs
 
-ols_model<- lm(valence ~ ., data=regressionDF,
+regressionDF_scaled<-as.data.frame(scale(training_data_8LVs, center = TRUE, scale = TRUE))
+regressionDF_scaled$valence<-target_data$Valence
+
+head(regressionDF)
+
+## Outlier detection
+boxplot(regressionDF)$out
+
+regressionDF_cleaned <- remove_outliers(regressionDF)
+
+regressionDF$valence<-target_data$Valence
+
+## Models
+ols_model<- lm(valence ~ ., data=regressionDF_scaled,
                x=TRUE, y = TRUE)
 
-ols_model_w<- lm(valence ~ ., data=regressionDF,
+ols_model_w<- lm(valence ~ ., data=regressionDF_scaled,
                x=TRUE, y = TRUE, weights = weights$weights)
 
 summary(ols_model)
@@ -104,34 +117,67 @@ summary(ols_model_w)
 layout(matrix(c(1,2,3,4),2,2))
 plot(ols_model_w)
 
-library(lmvar)
-cv.lm(ols_model, k = 10,seed=333)
+#### SVM MODELS
 
-cv.lm(ols_model_w, k = 10,seed=333)
+## tuning hyperparameters
+svmGrid <-  expand.grid(C = 10^(-3:2),
+                        degree = seq(1, 5, 1),
+                        scale = seq(0.1, 1, 0.1))
+                        #sigma = seq(0.1, 1, 0.1))
 
-set.seed(107)
-data_ctrl <- trainControl(method = "LOOCV", number = 10)
-model_caret <- train(valence ~ .,   # model to fit
-                     data = regressionDF,                        
+set.seed(29510)
+algo <- "lm"
+
+data_ctrl <- trainControl(method = "cv", p = 0.8, number = 10)
+
+model_caret <- train(form = valence ~ .,   # model to fit
+                     data = regressionDF, 
                      trControl = data_ctrl,              # folds
-                     weights = weights$weights,
-                     method = "lm",                      # specifying regression model
+                     method = algo,                      # specifying regression model
+                     #tuneGrid = svmGrid,
                      na.action = na.pass)
 
-model_caret
+model_caret_w <- train(form = valence ~ .,   # model to fit
+                     data = regressionDF,   
+                     trControl = data_ctrl,              # folds
+                     weights = weights$weights,
+                     method = algo,                      # specifying regression model
+                     #tuneGrid = svmGrid,
+                     na.action = na.pass)
+
+resamp <- resamples(list(model_caret, model_caret_w))
+summary(resamp)
+
+plot(model_caret_w)
+model_caret_w$finalModel
+
 capture.output(model_caret, file = "/Users/Tayfun/Desktop/Codes/OdorValPrediction/Plots/LinearRegression/MLRegression_Weighted_LOOCV.txt")
 
-## Manually LOOCV test
-#lwo <- rep(0,nrow(regressionDF))
+### TRY IN PYTHONNN WEIGHTINGG
+varImp(model_caret)
+varImp(model_caret_w)
 
-#for (j in 1:nrow(regressionDF))
-#{
-#  fj <- lm(valence~X0+X1+X2+X3+X4+X5+X6+X7, family=quasibinomial, data=regressionDF[-j,])
-#  lwo[j] <- predict(fj, regressionDF[j,], type="response")
-#}
+### Learning curve
+set.seed(29510)
 
-#caret::postResample(lwo, regressionDF$valence)
+lda_data <- learning_curve_dat(dat = regressionDF, 
+                              outcome = "valence",
+                              test_prop = 1/5,
+                              ## `train` arguments:
+                              method = "svmRadial", 
+                              metric = "RMSE",
+                              trControl = data_ctrl)
 
-#rss <- sum((lwo - regressionDF$valence) ^ 2)  ## residual sum of squares
-#tss <- sum((regressionDF$valence - mean(regressionDF$valence)) ^ 2)  ## total sum of squares
-#rsq <- 1 - rss/tss
+library(dplyr)
+target <- c('Testing', 'Training')
+lda_data <- filter(lda_data, Data %in% target)
+
+ggplot(lda_data, aes(x = Training_Size, y = RMSE, color = Data)) + 
+  geom_line() +
+  #geom_smooth(method = loess, span = .2) + 
+  ylim(0, 0.8) +
+  xlab ('Training size') +
+  theme_classic()
+
+
+
